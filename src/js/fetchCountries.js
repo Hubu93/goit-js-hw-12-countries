3484c0e6ea184f271a99ea6de30c1233648f99d8
Hubu93 <zshenialev2@gmail.com>
2021-10-26 11:45:58 +0300

    1

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 export default function fetchCountries(searchQuery) {
-    return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
-      .then(response => response.json())
-      .catch(error => alert(error));
-  }
+  return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+    .then(response => response.json())
+    .catch(error => alert(error));
+}
